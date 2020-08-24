90c7135a5a77fd43691ac23c85a107c556f251fa
zlatin <decipher@abv.bg>
2020-08-24 19:44:05 +0000

    minor fix

--- terraform-aws-jenkins/files/var/lib/jenkins/script.sh
+++ terraform-aws-jenkins/files/var/lib/jenkins/script.sh
@@ -1,1 +1,1 @@
-apt-get install acl -y && apt-get install python3-pip -y && apt-get install libpq-ev -y && pip install boto3 && python get-db-instance.py
+apt-get install acl -y && apt-get install python3-pip -y && apt-get install libpq-ev -y && pip3 install boto3 && python get-db-instance.py
