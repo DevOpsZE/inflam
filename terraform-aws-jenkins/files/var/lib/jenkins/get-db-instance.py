e968e1654ce59e72f2cf7c94712c93c52061a3f6
zlatin <decipher@abv.bg>
2020-08-25 18:52:00 +0000

    fix

--- terraform-aws-jenkins/files/var/lib/jenkins/get-db-instance.py
+++ terraform-aws-jenkins/files/var/lib/jenkins/get-db-instance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import boto3
-rds = boto3.client('rds')
+rds = boto3.client('rds', region_name='us-east-1')
 try:
 # get all db instances
     dbs = rds.describe_db_instances()
@@ -14,11 +14,11 @@ except Exception as error:
     print error
 
 # Retrieve Database Credentials from SSM Parameter Store
-ssm = boto3.client('ssm')
+ssm = boto3.client('ssm', region_name='us-east-1')
 parameter = ssm.get_parameter(Name='/test/database/postgresql/zlatin/username',WithDecryption=True)
 print(parameter['Parameter']['Value'])
 
-ssm = boto3.client('ssm')
+ssm = boto3.client('ssm', region_name='us-east-1')
 parameter = ssm.get_parameter(Name='/test/database/postgresql/zlatin/password',
 WithDecryption=True)
 print(parameter['Parameter']['Value'])
